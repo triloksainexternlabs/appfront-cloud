47f5ae6266dbb7e07467c1fb16d12bf48d15b7ee
Neetu Bairwa <neetubairwa@externlabs.com>
2021-01-07 17:05:38 +0530

    add progress bar on pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import {useState,useEffect} from 'react'
 import logos from './images/appLogo.svg';
 import ContinueButton from "./components/ContinueButton";
 import ElencoGaranzie from './pages/elencoGaranzie/ElencoGaranzie';
@@ -7,16 +8,36 @@ import DatiAnagrafici from "./pages/datiAnagrafici/datiAnagraficiContainer";
 import Questionario from "./pages/questionario/questionariocontainer";
 import RiepilogoGaranzieContainer from './pages/riepilogoGaranzie/riepilogoGaranzieContainer';
 import DatiContrato from "./pages/DatiContratto/datiContrattoContainer";
-
+import Progressbar from '../src/ProgressBar/progressbar'
 function App() {
-
+const [flag, setFlag] = useState(false);
+const [id, setId] = useState(1)
   const handleScroll = (value) => {
+    const hieght=window.innerHeigh;
+    setId(value)
+    console.log(window.innerHeight,'event hora h')
     window.scrollTo({
       top: window.innerHeight * value,
-      behavior: 'smooth'     
+      behavior: 'smooth'
     })
+    if (value === 1){
+      setFlag(false) 
+
+    }
+    else
+      setFlag(true)
   }
 
+  useEffect(() => {
+    window.addEventListener('scroll',(event)=>{
+      console.log(window.innerHeight,'event hora h')
+    })
+    return () => {
+      window.removeEventListener('scroll',(e)=>{
+        console.log(e,'removeListner')
+      })
+    }
+  }, [id])
   // if(true)
   // {
   //   return (
@@ -27,7 +48,7 @@ function App() {
   return (
     <div className="app">
       <div className="app-logo">
-        <img src={logos} alt="app-logo"/>
+        <img src={logos} alt="app-logo" />
       </div>
       <div className="step-view-container">
         <div>
@@ -35,31 +56,37 @@ function App() {
             <div>
               <span className="head-text">Gruppi Prodotto</span>
             </div>
-            <button className="btn-first" onClick={()=>handleScroll(6)}>
-                Prodotto Auto
+            <button className="btn-first" onClick={() => handleScroll(1)}>
+              Prodotto Auto
             </button>
           </div>
           <div className="form-two">
             <div className="head-text-w-underline">Elenco Prodotti</div>
-            <ProductListcontainer handleScroll={handleScroll}/>
+            <ProductListcontainer handleScroll={handleScroll} />
           </div>
+          <div className='product-container'>
+            <div className='product-list'>
           <div className="product-list-third">
             <div className="head-text-w-underline">Elenco Garanzie</div>
-              <ElencoGaranzie/>
-              <ContinueButton handleScroll={handleScroll} step={3} />
+              <ElencoGaranzie />
+            <ContinueButton handleScroll={handleScroll} step={3} />
           </div>
           <div className="product-list-third">
-            <DatiAnagrafici/>
-            <ContinueButton handleScroll={handleScroll} step={4}/>
+            <DatiAnagrafici />
+            <ContinueButton handleScroll={handleScroll} step={4} />
           </div>
           <div className="product-list-third">
-            <Questionario/>
-            <ContinueButton handleScroll={handleScroll} step={5}/>
+            <Questionario />
+            <ContinueButton handleScroll={handleScroll} step={5} />
           </div>
           <div className="product-list-third">
-            <DatiContrato/>
-            <ContinueButton handleScroll={handleScroll} step={6}/>
+            <DatiContrato />
+            <ContinueButton handleScroll={handleScroll} step={6} />
           </div>
+          </div>
+          <Progressbar id={id} flag={flag} />
+          </div>
+         
         </div>
       </div>
     </div>
